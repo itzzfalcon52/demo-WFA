b973a8462ee7be44bf2656e8ff640a2c55828b55
@itzzfalcon52 <hussaintaherkagalwala5253@gmail.com>
2025-09-20 18:32:05 +0530

    bugssss...

--- backend/main.py
+++ backend/main.py
@@ -244,4 +244,4 @@ def health():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # required for Render
-    uvicorn.run("fastapi_transformer_waf:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
